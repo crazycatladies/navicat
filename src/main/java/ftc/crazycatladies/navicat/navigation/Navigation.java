45edea603285ccb1c8b0ad9527945461befe7af9
Jason Gerstenberger <jason@gerstenberger.name>
2021-02-20 12:26:26 -0500

    meet 3 changes

--- src/main/java/ftc/crazycatladies/navicat/navigation/Navigation.java
+++ src/main/java/ftc/crazycatladies/navicat/navigation/Navigation.java
@@ -1,10 +1,14 @@
 package ftc.crazycatladies.navicat.navigation;
 
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
 import com.acmerobotics.roadrunner.drive.DriveSignal;
 import com.acmerobotics.roadrunner.followers.HolonomicPIDVAFollower;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
 import com.acmerobotics.roadrunner.trajectory.BaseTrajectoryBuilder;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
@@ -12,6 +16,7 @@ import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
 import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints;
 import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataResponse;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
 import java.util.Map;
@@ -25,6 +30,39 @@ import static ftc.crazycatladies.navicat.navigation.DriveConstantsProvider.TRACK
 import static java.lang.Math.toRadians;
 
 public class Navigation extends Subsystem {
+    class TurnContext {
+        double angle;
+        MotionProfile profile;
+
+        public TurnContext(double angle) {
+            this.angle = angle;
+        }
+
+        @Override
+        public String toString() {
+            return "TurnContext{" +
+                    "angle=" + angle +
+                    ", profile=" + profileToString() + " }";
+        }
+
+        @NotNull
+        private String profileToString() {
+            if (profile == null)
+                return "null";
+
+            return "{duration=" +
+                    profile.duration() +
+                    ", start=" +
+                    profile.start() +
+                    ", end=" +
+                    profile.end() +
+                    "}";
+        }
+    }
+
+    private final PIDFController turnController;
+    private final StateMachine<Trajectory> followSM;
+    private final StateMachine<TurnContext> turnSM;
     private MecanumDrive drive;
     private MecanumDriveBase driveBase;
     private Localization localization;
@@ -51,20 +89,48 @@ public class Navigation extends Subsystem {
         subsystems.add(drive);
         constraints = new MecanumConstraints(BASE_CONSTRAINTS, TRACK_WIDTH);
 
+        turnController = new PIDFController(HEADING_PID);
+        turnController.setInputBounds(0, 2 * Math.PI);
+
         followSM = new StateMachine<Trajectory>("FollowTrajectory");
         followSM.once((state, trajectory) -> {
             follower.followTrajectory(trajectory);
         });
         followSM.repeat((state, trajectory) -> {
-            if (!follower.isFollowing())
+            if (!follower.isFollowing()) {
+                driveBase.setDriveSignal(new DriveSignal());
                 state.next();
-            else {
+            } else {
                 driveBase.setDriveSignal(follower.update(currentPose));
                 lastError = follower.getLastError();
             }
         });
-        followSM.once((state, trajectory) -> {
-            driveBase.setDriveSignal(new DriveSignal());
+
+        turnSM = new StateMachine("TurnTo");
+        turnSM.once((state, context) -> {
+            double heading = currentPose.getHeading();
+            context.profile = MotionProfileGenerator.generateSimpleMotionProfile(
+                    new MotionState(heading, 0, 0, 0),
+                    new MotionState(heading + toRadians(context.angle), 0, 0, 0),
+                    constraints.maxVel, constraints.maxAccel);
+        });
+        turnSM.repeat((state, context) -> {
+            MotionState targetState = context.profile.get(state.getTimeInState().seconds());
+            if (state.getTimeInState().seconds() > context.profile.duration()
+                    && Math.abs(currentPose.getHeading() - targetState.getX()) < Math.toRadians(1.0)) {
+                driveBase.setDriveSignal(new DriveSignal());
+                state.next();
+            } else {
+                turnController.setTargetPosition(targetState.getX());
+                double correction = turnController.update(currentPose.getHeading());
+                double targetOmega = targetState.getV();
+                double targetAlpha = targetState.getA();
+                driveBase.setDriveSignal(new DriveSignal(new Pose2d(
+                        0, 0, targetOmega + correction
+                ), new Pose2d(
+                        0, 0, targetAlpha
+                )));
+            }
         });
     }
 
@@ -81,8 +147,6 @@ public class Navigation extends Subsystem {
     public void move(double forward, double turn, double strafe) {
         drive.move(forward, turn, strafe);
     }
-
-    private StateMachine<Trajectory> followSM;
 
     public TrajectoryBuilder trajectoryBuilder(double maxVel) {
         return trajectoryBuilder(maxVelConstraints(maxVel));
@@ -143,6 +207,9 @@ public class Navigation extends Subsystem {
     }
     public BaseTrajectoryBuilder splineTo(double x, double y, double heading) {
         return trajectoryBuilder().splineTo(new Vector2d(x, y), toRadians(heading), constraints);
+    }
+    public void turnBy(double angle) {
+        runSM(turnSM, new TurnContext(angle));
     }
     public BaseTrajectoryBuilder reverseTo(double x, double y, double heading) {
         return reverseTrajectoryBuilder().lineToLinearHeading(new Pose2d(x, y, toRadians(heading)), constraints);
