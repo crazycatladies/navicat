b1962dd93a29e7873ffb4614cdd1aed76ccaa374
Jason Gerstenberger <jason@gerstenberger.name>
2022-01-29 17:46:33 -0500

    fix DashboardUtil package

--- src/main/java/ftc/crazycatladies/navicat/navigation/DashboardUtil.java
+++ src/main/java/ftc/crazycatladies/navicat/navigation/DashboardUtil.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package ftc.crazycatladies.navicat.navigation;
 
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
